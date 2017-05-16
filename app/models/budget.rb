cce9a3b473f2344b38b689545fdeffc22a69c497
Kiru Karki <kiru.kiru28@gmail.com>
2017-05-16 22:49:57 +0630

    Add validations in Budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -29,6 +29,15 @@ class Budget < ApplicationRecord
 
   DEFAULT_DATE_FORMAT = '%d %b %Y'
 
+  ##
+  # Validations
+  #
+  validates :title, :original_amount, :start_date, :end_date, :remaining_amount, presence: true
+  validates :original_amount, :remaining_amount, :extra_used_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate  :is_remaining_mount_greater_than_original_amount
+  validate  :is_start_date_greater_than_end_date
+  validate  :allow_to_add_extra_used_amount_only_when_remaining_amount_is_zero
+
   def start_date
     if read_attribute(:start_date) != nil
       read_attribute(:start_date).strftime(DEFAULT_DATE_FORMAT)
@@ -48,4 +57,23 @@ class Budget < ApplicationRecord
     self.start_date         ||= Date.current.strftime(DEFAULT_DATE_FORMAT)
     self.end_date           ||= (Date.current + 1.day ).strftime(DEFAULT_DATE_FORMAT)
   end
+
+  private
+    def is_remaining_mount_greater_than_original_amount
+      if remaining_amount > original_amount
+          errors.add(:remaining_amount, %q(can't be greater than original amount))
+      end
+    end
+
+    def is_start_date_greater_than_end_date
+      if Date.parse(start_date) > Date.parse(end_date)
+        errors.add(:start_date, %q(can't be greather than end date))
+      end
+    end
+
+    def allow_to_add_extra_used_amount_only_when_remaining_amount_is_zero
+      if remaining_amount != 0 && extra_used_amount > 0
+        errors.add(:extra_used_amount, %q(remaining amount is not zero))
+      end
+    end
 end
