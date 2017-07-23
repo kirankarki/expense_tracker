4512b99879ea1b9c0d5ed57cdbea00e1d210a7eb
Kiru Karki <kiru.kiru28@gmail.com>
2017-07-23 22:45:21 +0630

    1. Use sidekiq for background jobs processing 2. Add sidekiq web dashboard 3. Add sidekiq config file to process searchkick jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root to: 'home#index'
-
+  mount Sidekiq::Web => '/sidekiq'  
+  
   devise_for :users
+  
   resources :budgets do
     resources :expenses
   end
