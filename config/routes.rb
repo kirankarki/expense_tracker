5e7545ae4a994689a8facba49df51bba5bd778ad
Kiru Karki <kiru.kiru28@gmail.com>
2017-09-24 21:59:25 +0630

    Do initial setup of active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   mount Sidekiq::Web => '/sidekiq'
 
   get 'search', to: 'search#search'
